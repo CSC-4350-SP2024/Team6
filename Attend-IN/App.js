336cb1a0adf6073af6dda3afa1e0fd5e80df9dae
Jawuah1 <jessekofia@hotmail.com>
2024-02-03 20:43:24 -0500

    Update dependencies to latest versions eslint

--- Attend-IN/App.js
+++ Attend-IN/App.js
@@ -1,19 +1,19 @@
+import React from 'react'
+import { GluestackUIProvider, Text, Box } from '@gluestack-ui/themed'
+import { config } from '@gluestack-ui/config' // Optional if you want to use default theme
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet } from 'react-native'
 
-import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed"
-import { config } from "@gluestack-ui/config" // Optional if you want to use default theme
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-
-export default function App() {
+export default function App () {
   return (
     <GluestackUIProvider config={config}>
-      <Box  style={styles.container}>
+      <Box style={styles.container}>
       <Text>Jesse, Tiffany, Bright, Karlissa, Tai!</Text>
       <StatusBar style="auto" />
     </Box>
     </GluestackUIProvider>
-    
-  );
+
+  )
 }
 
 const styles = StyleSheet.create({
@@ -21,6 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
