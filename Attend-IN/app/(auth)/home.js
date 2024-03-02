7dec3f403788bada09896c9a2f069fb80a8e3e12
Jawuah1 <jessekofia@hotmail.com>
2024-03-01 22:58:28 -0500

    Implemet Expo Get User Location Using Expo Location to request for the user to allow  us to use their location for further feature to be utilized.

--- Attend-IN/app/(auth)/home.js
+++ Attend-IN/app/(auth)/home.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Text, View, ImageBackground, Image, Pressable } from 'react-native';
-import { Link } from 'expo-router';
-import styles from './styles';
-import { useFonts } from "expo-font";
+import React from 'react'
+import { Text, View, ImageBackground, Image, Pressable } from 'react-native'
+import { Link } from 'expo-router'
+import styles from './styles'
+import { useFonts } from 'expo-font'
 
-export default function Page() {
+export default function Page () {
   const [fontsLoaded] = useFonts({
-    'Tinos': require('../../assets/fonts/Tinos-Regular.ttf'),
-  });
+    Tinos: require('../../assets/fonts/Tinos-Regular.ttf')
+  })
   if (!fontsLoaded) {
-    return <Text>...</Text>;
+    return <Text>...</Text>
   }
-  
+
   return (
     <View style={styles.imageContainer}>
       <ImageBackground source={require('../../assets/GSU Campus.jpg')} style={styles.imageBackground}>
         <View style={styles.container}>
           <Text style={styles.title}>Attend-In</Text>
           <Image source={require('../../assets/Panther Head Logo.png')} style={styles.logo}/>
-          <View style={[styles.verticallySpaced, {marginTop: 60}]}>
+          <View style={[styles.verticallySpaced, { marginTop: 60 }]}>
             <Link href="/login" asChild>
             <Pressable style={styles.landingButton}>
                 <Text style={styles.buttonText}>Sign in</Text>
@@ -28,5 +28,5 @@ export default function Page() {
         </View>
       </ImageBackground>
     </View>
-  );
+  )
 }
